953bab268cf096eba49729ee4ce7e0090525a3bc
Satish Junghare <satish8264@gmail.com>
2019-05-24 18:40:16 +0530

    python variables

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 # price is variable name & 10 is integer value assigned to variable
 price = 10
 rating = 4.5
-name = 'Satish Junghare'
+name = 'Satish'
 is_published = False # remember boolean True or False must be first later is capital
 
 # print() will print the value of variable price
